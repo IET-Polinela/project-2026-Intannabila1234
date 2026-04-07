fa9c7f89f00158670bfdb7e70926ea5fdb687879
intan nabila <inabilla244@gmail.com>
2026-04-07 16:46:08 +0700

    push tugas

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,40 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Report
+from .forms import ReportForm
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('main_app.urls')),
-]
+# READ
+def home(request):
+    reports = Report.objects.all()
+    return render(request, 'main_app/home.html', {'reports': reports})
+
+
+# CREATE
+def add_report(request):
+    if request.method == "POST":
+        form = ReportForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = ReportForm()
+    return render(request, 'main_app/add_report.html', {'form': form})
+
+
+# UPDATE
+def update_report(request, id):
+    report = get_object_or_404(Report, id=id)
+    if request.method == "POST":
+        form = ReportForm(request.POST, instance=report)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = ReportForm(instance=report)
+    return render(request, 'main_app/update_report.html', {'form': form})
+
+
+# DELETE
+def delete_report(request, id):
+    report = get_object_or_404(Report, id=id)
+    report.delete()
+    return redirect('home')
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Report
+from .forms import ReportForm
 
-# Create your views here.
-def home(request):
-    return render(request, 'home.html')
+# READ (List)
+def report_list(request):
+    reports = Report.objects.all()
+    return render(request, 'report_list.html', {'reports': reports})
+
+
+# CREATE
+def add_report(request):
+    if request.method == 'POST':
+        form = ReportForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('report_list')
+    else:
+        form = ReportForm()
+    return render(request, 'add_report.html', {'form': form})
+
+
+# UPDATE
+def edit_report(request, id):
+    report = get_object_or_404(Report, id=id)
+    if request.method == 'POST':
+        form = ReportForm(request.POST, instance=report)
+        if form.is_valid():
+            form.save()
+            return redirect('report_list')
+    else:
+        form = ReportForm(instance=report)
+    return render(request, 'add_report.html', {'form': form})
+
+
+# DELETE
+def delete_report(request, id):
+    report = get_object_or_404(Report, id=id)
+    report.delete()
+    return redirect('report_list')
